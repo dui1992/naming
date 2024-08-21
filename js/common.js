d7dd19bc675d9466cc799d962fb367cac6db065b
dui <jhdw77@gmail.com>
2024-08-21 14:02:28 +0900

    sub

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
 $(function () {
   $('#header').load('common/header.html');
   $('#footer').load('common/footer.html');
-
+  
 });
+
+
+$(function () {
+  $('.item #add-cart').click(function () {
+    $('.popup').addClass('on');
+  });
+  $('.popup #cencel_btn').click(function () {
+    $('.popup').removeClass('on');
+  });
+});
+
+
